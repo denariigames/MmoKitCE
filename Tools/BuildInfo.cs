d267418c5292f2560973efb64e5352845ed8aa39
John Money <jemoney@protonmail.com>
2026-05-18 16:31:55 -0400

    remove menu

--- Tools/BuildInfo.cs
+++ Tools/BuildInfo.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "MmoKitCE/Tools/BuildInfo", fileName = "BuildInfo")]
 public class BuildInfo : ScriptableObject
 {
     [Tooltip("Current version shown to players")]
