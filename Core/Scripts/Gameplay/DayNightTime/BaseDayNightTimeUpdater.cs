f332807bfa950bfdbcb3aedb3a2a277041777457
John Money <jemoney@protonmail.com>
2026-06-17 14:43:07 -0400

    Add day and time to WorldSaveData #117

--- Core/Scripts/Gameplay/DayNightTime/BaseDayNightTimeUpdater.cs
+++ Core/Scripts/Gameplay/DayNightTime/BaseDayNightTimeUpdater.cs
@@ -7,6 +7,10 @@ namespace MultiplayerARPG
     public abstract class BaseDayNightTimeUpdater : ScriptableObject
     {
         public float TimeOfDay { get; protected set; }
+        public int CurrentDay { get; protected set; }
+
+        [Header("Time Settings")]
+        [SerializeField] [Range(0, 23)] private int defaultHourOfDay = 9;
 
         /// <summary>
         /// Init day of time, this function will be called at server to init time of day.
@@ -30,6 +34,22 @@ namespace MultiplayerARPG
         public virtual void SetTimeOfDay(float timeOfDay)
         {
             TimeOfDay = timeOfDay;
+        }
+
+        /// <summary>
+        /// Implements loading from local worldSaveData
+        /// </summary>
+        /// <param name="worldSaveData"></param>
+        public virtual void LoadFromWorldSaveData(WorldSaveData worldSaveData)
+        {
+            if (worldSaveData == null) return;
+
+            //Set default if day and hour are 0
+            if (worldSaveData.currentDay == 0 && worldSaveData.timeOfDay <= 0f)
+                worldSaveData.timeOfDay = defaultHourOfDay;
+
+            TimeOfDay = Mathf.Clamp(worldSaveData.timeOfDay, 0f, 24f);
+            CurrentDay = worldSaveData.currentDay;
         }
     }
 }
--- Core/Scripts/WorldData/SerializationSurrogates/WorldSaveDataSerializationSurrogate.cs
+++ Core/Scripts/WorldData/SerializationSurrogates/WorldSaveDataSerializationSurrogate.cs
@@ -13,6 +13,8 @@ namespace MultiplayerARPG
         {
             WorldSaveData data = (WorldSaveData)obj;
             info.AddListValue("buildings", data.buildings);
+            info.AddValue("timeOfDay", data.timeOfDay);
+            info.AddValue("currentDay", data.currentDay);
         }
 
         public object SetObjectData(
@@ -23,6 +25,23 @@ namespace MultiplayerARPG
         {
             WorldSaveData data = (WorldSaveData)obj;
             data.buildings = new List<BuildingSaveData>(info.GetListValue<BuildingSaveData>("buildings"));
+
+            // Sync time and day
+            try
+            {
+                data.timeOfDay = info.GetSingle("timeOfDay");
+            }
+            catch { data.timeOfDay = 0; }
+
+            try
+            {
+                data.currentDay = info.GetInt32("currentDay");
+            }
+            catch { data.currentDay = 0; }
+
+            if (GameInstance.Singleton.DayNightTimeUpdater != null)
+                GameInstance.Singleton.DayNightTimeUpdater.LoadFromWorldSaveData(data);
+
             obj = data;
             return obj;
         }
